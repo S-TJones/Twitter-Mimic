81af758936b52ea46b308efd8f2e2c9dd711f273
S-TJones <jones.shemar.sj@gmail.com>
2022-06-24 15:56:54 -0500

    More aside changes

--- src/components/aside-menu/aside-menu.vue
+++ src/components/aside-menu/aside-menu.vue
@@ -2,6 +2,19 @@
 <script lang="ts" src="./aside-menu.ts"></script>
 <template>
   <div class="[ aside-menu ]">
-    <h1>aside-menu</h1>
+    <div>
+      <img src="" alt="" srcset="">
+    </div>
+    <div class="[ flex flex-col ]">
+      <a href="#" class="[  ]">Home</a>
+      <a href="#" class="[  ]">Explore</a>
+      <a href="#" class="[  ]">Notifications</a>
+      <a href="#" class="[  ]">Messages</a>
+      <a href="#" class="[  ]">Bookmarks</a>
+      <a href="#" class="[  ]">Lists</a>
+      <a href="#" class="[  ]">Profile</a>
+      <a href="#" class="[  ]">More</a>
+      <b-button rounded>Tweet</b-button>
+    </div>
   </div>
 </template>
