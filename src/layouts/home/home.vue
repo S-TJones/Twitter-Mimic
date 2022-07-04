a27a02ddc1fb20081ded35ab6bad28fc1f367f84
S-TJones <jones.shemar.sj@gmail.com>
2022-07-04 15:41:38 -0500

    Worked on the Trends

--- src/layouts/home/home.vue
+++ src/layouts/home/home.vue
@@ -2,7 +2,7 @@
 <script lang="ts" src="./home.ts"></script>
 <template>
 
-<div class="[ home ][ flex flex-row ]">
+<div class="[ home ][ flex flex-row ]" style="background-color: white;">
   <aside class="[ w-1/4 h-full ][ border border-twittergray ][ flex justify-center items-center ]">
     <AsideMenu customProp="" />
   </aside>
@@ -15,11 +15,75 @@
     </transition>
 
     <div class="[ w-1/3 h-full ][ border border-twittergray ][ flex flex-col ]">
-      <div>
+      <div class="[ p-4 ]">
         <h3>Trends for you</h3>
+        <div class="trend [ flex flex-row ][ p-4 ]">
+          <div class="[ flex flex-col ]">
+            <p>Only on Twitter · Trending</p>
+            <p class="[ bold ]">WhatsApp</p>
+            <p>209K Tweets</p>
+          </div>
+          <div>...</div>
+        </div>
+        <div class="trend [ flex flex-row ][ p-4 ]">
+          <div class="[ flex flex-col ]">
+            <p>Gaming · Trending</p>
+            <p class="[ bold ]">#CallOfDuty</p>
+            <p>4,158 Tweets</p>
+          </div>
+          <div>...</div>
+        </div>
+        <div class="trend [ flex flex-row ][ p-4 ]">
+          <div class="[ flex flex-col ]">
+            <p>Trending in Jamaica</p>
+            <p class="[ bold ]">Portmore</p>
+            <p></p>
+          </div>
+          <div>...</div>
+        </div>
+        <a href="#" class="[ no-underline ][ p-4 ]">Show more</a>
       </div>
-      <div>
+      <div class="[ p-4 ]">
         <h3>Who to follow</h3>
+        <!--  -->
+        <div class="follow [ flex flex-row justify-between ][ py-4 ]">
+          <div>
+            <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;">
+          </div>
+          <div class="[ flex flex-col ]">
+            <p>Steam Support</p>
+            <p>@Steam_Support</p>
+          </div>
+          <div class="[ ml-auto ]">
+            <button>Follow</button>
+          </div>
+        </div>
+        <!--  -->
+        <div class="follow [ flex flex-row ][ py-4 ]">
+          <div>
+            <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;">
+          </div>
+          <div class="[ flex flex-col ]">
+            <p>Disney</p>
+            <p>@Disney</p>
+          </div>
+          <div class="[ ml-auto ]">
+            <button>Follow</button>
+          </div>
+        </div>
+        <!--  -->
+        <div class="follow [ flex flex-row justify-between ][ py-4 ]">
+          <div>
+            <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;">
+          </div>
+          <div class="[ flex flex-col ]">
+            <p>Elon Musk</p>
+            <p>@elonmusk</p>
+          </div>
+          <div class="[ ml-auto ]">
+            <button>Follow</button>
+          </div>
+        </div>
       </div>
 
     </div>
--- src/pages/dash/dash.vue
+++ src/pages/dash/dash.vue
@@ -6,12 +6,12 @@
       <p class="[ text-base ]">Home</p>
       <i class="[ h-8 w-12 ]"></i>
     </div>
-    <form class="[ pt-1 ]">
+    <form class="[ pt-1 ][ w-full ]">
       <div class="[  ]">
         <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ rounded-full h-16 w-16 ][ flex items-center justify-center ]">
       </div>
       <div class="[  ]">
-        <textarea name="tpost" id="tpost" cols="30" rows="3" placeholder="What's happening?"></textarea>
+        <textarea name="tpost" id="tpost" cols="30" rows="3" placeholder="What's happening?" class="[ w-full ]"></textarea>
         <div class="[  ]">
           <div class="[ flex flex-row justify-evenly ]">
             <i></i>
@@ -19,16 +19,102 @@
             <i></i>
             <i></i>
           </div>
-          <!-- Switch this button with a Buefy button -->''
+          <!-- Switch this button with a Buefy button -->
           <button type="submit">Tweet</button>
         </div>
       </div>
     </form>
     <hr>
     <div class="[ my-1 ]"></div>
-    <div class="post [ h-full max-w-xl ][ mx-auto ]">
-      <div class="[ border-b ]">
-
+    <!--  -->
+    <div class="post [ w-full max-w-xl ][ mx-auto ][ text-sm ][ border ]">
+      <div class="[ px-4 ][ w-full ]">
+        <div class="[ flex flex-col flex-shrink flex-grow ]">
+          <div class="[ flex flex-col ]">
+            <div class="[ pt-3 ][ mb-1 ]"></div>
+            <div class="[ flex flex-row ]">
+              <div class="[ mr-3 ][ flex-shrink-0 flex justify-center ]">
+                <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;">
+              </div>
+              <div class="[ pb-3 ][ flex flex-col justify-center ]">
+                <div class="[ mb-1 ][ flex flex-row justify-between items-start ]">
+                  <div>Marvel Entertainment <i></i> @Marvel · 23h</div>
+                  <div class="[ ml-5 ][ h-5 ][ flex justify-center items-center text-center ]">...</div>
+                </div>
+                <div>
+                  <div class="post-text">
+                    In 5 days, witness the power of #Thor. <br>
+                    Experience the cinematic event, Marvel Studios’ #ThorLoveAndThunder, only in theaters this Friday! Get tickets now: http://fandango.com/ThorLoveAndThunder
+                  </div>
+                  <div class="post-image [ mt-3 ]">
+                    <div><img src="" alt="" class="[  ]"></div>
+                    <div>From Thor</div>
+                  </div>
+                  <div class="post-buttons [ flex flex-row justify-between ][ mt-3 ]">
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">33</p>
+                    </div>
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">247</p>
+                    </div>
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">2465</p>
+                    </div>
+                    <div class="[ flex justify-start ]"><i></i></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="post [ w-full max-w-xl ][ mx-auto ][ text-sm ][ border ]">
+      <div class="[ px-4 ][ w-full ]">
+        <div class="[ flex flex-col flex-shrink flex-grow ]">
+          <div class="[ flex flex-col ]">
+            <div class="[ pt-3 ][ mb-1 ]"></div>
+            <div class="[ flex flex-row ]">
+              <div class="[ mr-3 ][ flex-shrink-0 flex justify-center ]">
+                <img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;">
+              </div>
+              <div class="[ pb-3 ][ flex flex-col justify-center ]">
+                <div class="[ mb-1 ][ flex flex-row justify-between items-start ]">
+                  <div>Marques Brownlee <i></i> @MKBHD · 13h</div>
+                  <div class="[ ml-5 ][ h-5 ][ flex justify-center items-center text-center ]">...</div>
+                </div>
+                <div>
+                  <div class="post-text">
+                    Yes yes you should definitely take lots of videos of these fireworks. You will definitely watch the videos later
+                  </div>
+                  <div class="post-image [ mt-3 ]">
+                    <div><img src="@/assets/twitter/twitter_logo.jpg" alt="user_profile_pic" class="[ h-12 w-12 ][ flex items-center justify-center ]" style="border-radius: 50%;"></div>
+                    <div></div>
+                  </div>
+                  <div class="post-buttons [ flex flex-row justify-between ][ mt-3 ]">
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">{{temp}}343</p>
+                    </div>
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">1984</p>
+                    </div>
+                    <div class="[ flex justify-start ]">
+                      <i></i>
+                      <p class="[ px-3 ]">37.8K</p>
+                    </div>
+                    <div class="[ flex justify-start ]"><i></i></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
